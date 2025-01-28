dee002d22c48287247142ea68b3f4487c86cc26e
darmawan suhara <dsuharaaa@gmail.com>
2025-01-28 14:13:50 +0700

    ualng dari awal

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import numpy as np
 from PIL import Image
 
 # Memuat model yang sudah dilatih
-model = load_model('batikdarma/main/model/BatikDetection.h5', compile=False)
+model = load_model('model/BatikDetection.h5', compile=False)
 
 # Mendefinisikan label berdasarkan kelas yang digunakan saat pelatihan
 labels = ["Batik Betawi", "Batik Kawung", "Batik Megamendung", "Batik Parang", "Batik Sekar Jagad"]
